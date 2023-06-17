f6064fdb6a3ebe4b02a8643272e94097e38edc64
macmajewski <macmajewski@outlook.com>
2023-06-17 04:20:24 +0000

    Static Export

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    // Static Export
+    // https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+    output: 'export',
+}
 
 module.exports = nextConfig
