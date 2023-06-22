b6efa6217b20f63511638ab46a7689ccfdae2d5e
Maciej Majewski <macmajewski@outlook.com>
2023-06-21 19:59:51 -0700

    Refactor

--- src/app/components/eventCard.js
+++ src/app/components/eventCard.js
@@ -1,14 +1,24 @@
+import Image from 'next/image'
 import styles from './eventCard.module.css'
 
-export default function EventCard(event) {
-    return (<a href={event.link} title={event.name} className={styles.event_card + ' event'}>
+export default function EventCard({ event }) {
+    return (<a href={event.link} title={event.name} className={styles.event_card}>
         <small>{event._timeFormatted}</small>
         <strong className={styles.event_name}>{formatName(event.name)}</strong>
         <small className={styles.event_yes_rsvp}>{event.yes_rsvp_count} going</small>
     </a>);
 }
 
-function formatName(name, maxLength = 32) {
+export function LinkCard({ href, label }) {
+    return (<a href={href} title={label} className={styles.link_card}>
+        <Image width={40} height={40} src="/calendar-week.svg" 
+            className={styles.link_image}
+            alt="Calendar icon" />
+        {label}
+    </a>);
+}
+
+function formatName(name, maxLength = 60) {
     return name.length > maxLength
         ? `${name.substr(0, maxLength - 3)}...`
         : name;
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,11 @@
 import Image from 'next/image'
 import { upcoming } from './data/meetup'
-import EventCard from './components/eventCard'
+import EventCard, { LinkCard } from './components/eventCard'
 import styles from './page.module.css'
+
+// todo: Move const somewhere else
+const GROUP_URL = 'https://www.meetup.com/northwest-trail-runners';
+const EVENTS_URL = `${GROUP_URL}/events`;
 
 export default async function Home() {
     const events = await upcoming();
@@ -13,8 +17,7 @@ export default async function Home() {
                 <div className={styles.intro_container + ' container'}>
                     <h1><span className={styles.intro_header__nw}>Northwest</span> Trail Runners</h1>
                     <p>Community based in Portland, OR.</p>
-                    <a href="https://www.meetup.com/northwest-trail-runners"
-                        role="button" className={styles.intro_button + ' font-serif'}>
+                    <a href={GROUP_URL} role="button" className={styles.intro_button + ' font-serif'}>
                         Join us on Meetup
                     </a>
                 </div>
@@ -22,10 +25,8 @@ export default async function Home() {
 
             <section className={styles.events_section}>
                 <div className={styles.events_background_transition}>
-                    <Image
-                        fill
-                        src="/wildwood-elevation-chart.svg"
-                    />
+                    <Image fill src="/wildwood-elevation-chart.svg" 
+                        alt="Background transition" />
                 </div>
                 <div className={styles.events_background_texture}></div>
 
@@ -33,19 +34,17 @@ export default async function Home() {
                     <div className={styles.events_header}>
                         <small className={styles.events_label}>Events</small>
                         <h2>Trail runs are posted weekly</h2>
-                        {/* todo: Remove commented element
-                         <p>Lorem ipsum dolor sit amet</p> */}
                     </div>
 
                     {events.length > 0 && <div className={styles.events_scroller}>
-                    <div className={styles.events_cards}>
-                        {events.map(event => EventCard(event))}
-                        <article className="event">
-                            View all
-                        </article>
-                        <div className={styles.events_spacer}></div>
-                    </div>
-                </div>}
+                        <div className={styles.events_grid}>
+                            {events.map(event => <span className={styles.events_column}>
+                                <EventCard event={event} /></span>)}
+                            <span className={styles.events_column}>
+                                <LinkCard href={EVENTS_URL} label="View events on Meetup" />
+                            </span>
+                        </div>
+                    </div>}
                 </div>
             </section>
 
