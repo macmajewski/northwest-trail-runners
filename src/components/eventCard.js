3573eefe9c9d4d3cc1b44658ab1c522783d34c59
Maciej Majewski <macmajewski@outlook.com>
2023-06-23 17:43:45 -0700

    Event going avatars

--- src/components/eventCard.js
+++ src/components/eventCard.js
@@ -1,26 +1,39 @@
-import Image from 'next/image'
 import styles from './eventCard.module.css'
 
 export default function EventCard({ event }) {
-    return (<a href={event.link} title={event.name} className={styles.event_card}>
+    return (<a href={event.link} title={event.name} className={styles.card}>
         <small>{event._timeFormatted}</small>
-        <strong className={styles.event_name}>{formatName(event.name)}</strong>
-        <small className={styles.event_yes_rsvp}>{event.yes_rsvp_count} going</small>
-        <span className={styles.event_background} 
+        <strong className={styles.name}>{truncate(event.name)}</strong>
+        <Going count={event.yes_rsvp_count} />
+        <span className={styles.background} 
             style={{ backgroundImage: `url(/event/venue/${event.id})` }}></span>
     </a>);
 }
 
-export function LinkCard({ href, label }) {
-    return (<a href={href} title={label} className={styles.link_card}>
-        <Image width={40} height={40} src="/calendar-week.svg" 
-            className={styles.link_image}
-            alt="Calendar icon" />
-        {label}
-    </a>);
+function Going({ count }) {
+    return (<small className={styles.going}>
+        <Avatars count={count} />
+        {count} going
+    </small>);
 }
 
-function formatName(name, maxLength = 46) {
+function Avatars({ count }) {
+    return (<span className={styles.avatars}>
+        {Array.from(Array(Math.min(count, 3)), (_, i) => 
+            (<span className={styles.avatar + ` ${color()}`}></span>))}
+    </span>);
+}
+
+function color() {
+    const val = Math.floor(Math.random() * 3);
+    if (val === 0)
+        return styles.yellow;
+    if (val === 1)
+        return styles.green;
+    return styles.pink;
+}
+
+function truncate(name, maxLength = 46) {
     return name.length > maxLength
         ? `${name.substr(0, maxLength - 3)}...`
         : name;
