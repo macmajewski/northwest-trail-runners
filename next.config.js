8feb354eecfc6f5eb3485a2f31ca41f4620a25f3
macmajewski <macmajewski@outlook.com>
2025-03-08 15:57:36 -0800

    Revert

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    // Static Export
-    // https://nextjs.org/docs/app/building-your-application/deploying/static-exports
-    // output: 'export',
-}
-
-module.exports = nextConfig
