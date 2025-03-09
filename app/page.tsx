0ff32cb18285fb4ed8e4807e1bbfba6560997c86
macmajewski <macmajewski@outlook.com>
2025-03-09 12:10:09 -0700

    Header take 2

--- app/page.tsx
+++ app/page.tsx
@@ -33,8 +33,8 @@ export default async function Home() {
 
                 <div className="container">
                     <div className={styles.events_header}>
-                        <small className={styles.events_label}>Upcoming Runs</small>
-                        <h2>New trails, new challenges</h2>
+                        <small className={styles.events_label}>Trail Time</small>
+                        <h2>Upcoming Events</h2>
                     </div>
 
                     {events.length > 0 && <div className={styles.events_scroller}>
