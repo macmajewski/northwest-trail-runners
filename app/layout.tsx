b226f7f98fdc7c182fa1242d73318c8796cf6172
macmajewski <macmajewski@outlook.com>
2025-03-08 21:30:07 -0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,18 @@
-import type {Metadata} from 'next';
-import '@picocss/pico'
+import type {Metadata} from "next";
+import {ReactNode} from "react";
+import siteConfig from "@/contants/siteConfig";
+import "@picocss/pico";
 import "./globals.css";
 
 export const metadata: Metadata = {
-    title: 'Northwest Trail Runners',
-    description: 'Trail running community based in Portland, OR. Come join us!',
+    title: siteConfig.TITLE,
+    description: siteConfig.DESCRIPTION,
 };
 
-export default function RootLayout({ children }: Readonly<{ children: React.ReactNode; }>) {
+export default function RootLayout({children}: Readonly<{ children: ReactNode }>) {
     return (
         <html lang="en" data-theme="light">
-            <body>{children}</body>
+        <body>{children}</body>
         </html>
     );
 }
