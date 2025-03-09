0f397d81b45b6250e3ab6a783b8230e66e7a3b21
macmajewski <macmajewski@outlook.com>
2025-03-08 21:29:47 -0800

    Create siteConfig.ts

---
+++ contants/siteConfig.ts
@@ -0,0 +1,7 @@
+const siteConfig = {
+    TITLE: "Northwest Trail Runners",
+    DESCRIPTION: "Trail running community based in Portland, OR. Come join us!",
+    MEETUP_GROUP_URL: "https://www.meetup.com/northwest-trail-runners",
+} as const;
+
+export default siteConfig;
