94a4a62dab697697ee14e959c1cfae39fc033ead
macmajewski <macmajewski@outlook.com>
2025-03-08 21:29:42 -0800

    Add Event interface

--- services/meetup.ts
+++ services/meetup.ts
@@ -1,10 +1,69 @@
-import moment from 'moment-timezone'
+import moment from "moment-timezone";
 
-const BASE_URL = 'https://api.meetup.com/northwest-trail-runners';
+const BASE_URL = "https://api.meetup.com/northwest-trail-runners";
+
+export interface Event {
+    created: number
+    duration: number
+    id: string
+    name: string
+    rsvp_limit: number
+    date_in_series_pattern: boolean
+    status: string
+    time: number
+    local_date: string
+    local_time: string
+    updated: number
+    utc_offset: number
+    waitlist_count: number
+    yes_rsvp_count: number
+    venue: Venue
+    is_online_event: boolean
+    eventType: string
+    group: Group
+    link: string
+    description: string
+    how_to_find_us: string
+    visibility: string
+    member_pay_fee: boolean,
+
+    // Custom field
+    _timeFormatted: string
+}
+
+export interface Venue {
+    id: number
+    name: string
+    lat: number
+    lon: number
+    repinned: boolean
+    address_1: string
+    city: string
+    country: string
+    localized_country_name: string
+    zip: string
+    state: string
+}
+
+export interface Group {
+    created: number
+    name: string
+    id: number
+    join_mode: string
+    lat: number
+    lon: number
+    urlname: string
+    who: string
+    localized_location: string
+    state: string
+    country: string
+    region: string
+    timezone: string
+}
 
 // todo: Cache in case API is down
 // https://nextjs.org/docs/app/building-your-application/data-fetching/caching
-export async function fetchEvents() {
+export async function fetchEvents(): Promise<Event[]> {
     const response = await fetch(`${BASE_URL}/events`, {
         next: { revalidate: 60 }
     });
@@ -19,12 +78,15 @@ export async function fetchEvents() {
     return Array.isArray(result) ? result : [];
 }
 
-// Fetch public events from Meetup (top 4 by default)
-export async function upcoming(take = 4) {
+/**
+ * Fetch public upcoming events from Meetup (top 4 by default)
+ * @param take
+ */
+export async function fetchUpcomingEvents(take = 4): Promise<Event[]> {
     const events = await fetchEvents();
 
     return events
-        .filter(event => event.visibility === 'public')
+        .filter(event => event.visibility === "public")
         .sort((a, b) => a.time - b.time)
         .splice(0, take)
         .map(event => {
