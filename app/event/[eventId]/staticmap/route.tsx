8713a1869e972024ce3697ceea42f735f5488989
macmajewski <macmajewski@outlook.com>
2025-03-08 22:04:16 -0800

    Improve error handling around static maps

--- app/event/[eventId]/staticmap/route.tsx
+++ app/event/[eventId]/staticmap/route.tsx
@@ -1,26 +1,45 @@
 import {ImageResponse} from "next/og";
 import {fetchEvents} from "@/services/meetup";
 
-const GEOAPIFY_URL = "https://maps.geoapify.com/v1/staticmap";
+// Geoapify settings
+const URL = "https://maps.geoapify.com/v1/staticmap";
 const KEY = process.env.GEOAPIFY_KEY;
 const WIDTH = 300;
 const HEIGHT = 500;
+const STYLE = "toner";
+const ZOOM = 14;
 
 export async function GET(request: Request, {params}: { params: Promise<{ eventId: string }> }) {
     const {eventId} = await params;
 
-    const events = await fetchEvents();
-    const event = events.find(e => e.id == eventId);
+    try {
+        if (!KEY)
+            throw new Error("Geoapify key not configured.");
 
-    if (event && event.venue) {
-        const v = event.venue;
-        const url = `${GEOAPIFY_URL}?style=toner&width=${WIDTH}&height=${HEIGHT}&zoom=14&center=lonlat:${v.lon},${v.lat}&apiKey=${KEY}`;
+        const events = await fetchEvents();
+        const event = events.find(e => e.id == eventId);
 
-        const response = await fetch(url, { next: { revalidate: 3600 } });
+        if (!event)
+            throw new Error("Event not found.");
+
+        if (!event.venue)
+            throw new Error("Venue not provided.");
+
+        const { lon, lat } = event.venue;
+        if (!lon || !lat)
+            throw new Error("Venue lon/lat not provided.");
+
+        const url = `${URL}?center=lonlat:${lon},${lat}&width=${WIDTH}&height=${HEIGHT}&style=${STYLE}&zoom=${ZOOM}&apiKey=${KEY}`;
+
+        const response = await fetch(url, {next: {revalidate: 3600}});
+        if (!response.ok)
+            throw new Error(`Failed to fetch image. Status: ${response.status}. URL: ${url}`);
+
         const blob = await response.blob();
-
         return new Response(blob);
-    } else {
+    } catch (error) {
+        console.error(error);
+
         // todo: Fallback image
         return new ImageResponse(<></>);
     }
